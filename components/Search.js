e88e7eaa3f8da6c59f6f648008b3d501dacc3b0d
hissamumair <hissamyousafzai@gmail.com>
2024-02-24 19:03:02 +0500

    colors

--- components/Search.js
+++ components/Search.js
@@ -8,7 +8,7 @@ export default function Search({ text, setText }) {
         onChangeText={(text) => setText(text)}
         value={text}
         placeholder="Searchdfffhh"
-        placeholderTextColor={"black"}
+        placeholderTextColor={"white"}
       />
     </View>
   );
